c443f74f8ebae2e9b95190e5fbee98f9eaee745f
walák <feniksy@gmail.com>
2016-03-08 00:43:51 +0100

    Tests pushed to 100% coverage

--- src/test/java/com/github/walak/doubleoptional/DoubleOptionalMiscTest.java
+++ src/test/java/com/github/walak/doubleoptional/DoubleOptionalMiscTest.java
@@ -1,6 +1,24 @@
 package com.github.walak.doubleoptional;
 
-public class DoubleOptionalMiscTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+public class DoubleOptionalMiscTest extends DoubleOptionalBasicTest {
+
+    private final DoubleOptional<Integer, String> firstOptional = DoubleOptional.ofOptionals(optionalA, optionalB);
+    private final DoubleOptional<Integer, String> secondOptional = DoubleOptional.ofValues(valueA, valueB);
+
+    @Test
+    public void shouldBeEqualForTheSameValuesWithin() {
+        assertEquals(firstOptional, firstOptional);
+        assertNotEquals(firstOptional, new Object());
+        assertEquals(firstOptional, secondOptional);
+    }
+
+    @Test
+    public void shouldReturnTheSameHashcodeForTheSameValuesWithin() {
+        assertEquals(firstOptional.hashCode(), secondOptional.hashCode());
+    }
 }
